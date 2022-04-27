57e4fefca01e8b2e3f6edbc85813e734a2b264cc
Josh Munday <97485231+JDM210@users.noreply.github.com>
2022-04-27 15:08:49 -0400

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace joshinherit
+{
+    public class User
+    {
+        public string Name;
+        private string Location;
+        public User()
+        {
+            Console.WriteLine("Inheritance Assignment");
+        }
+        public void GetUserInfo(string loc)
+        {
+            Location = loc;
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Location: {0}", Location);
+        }
+    }
+    public class Info : User
+    {
+        public int Age;
+        public Info()
+        {
+            Console.WriteLine("OOP 2022");
+        }
+        public void GetAge()
+        {
+            Console.WriteLine("Age: {0}", Age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info d = new Info();
+            d.Name = "Josh Munday";
+            d.Age = 29;
+            d.GetUserInfo("Bradford, PA");
+            d.GetAge();
+            Console.WriteLine("\nThank You");
+            Console.ReadLine();
+        }
+    }
+}
